705435fa3c93a9c1b1a56b8a0a67e31ac9dd69cd
mazrhiyo <mazrhiyo@gmail.com>
2019-10-18 17:53:13 +0700

    tes

---
+++ about.php
@@ -0,0 +1,93 @@
+<!--Template Name: vacayhome
+File Name: about.html
+Author Name: ThemeVault
+Author URI: http://www.themevault.net/
+License URI: http://www.themevault.net/license/-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="images/icons/logo.png"/>
+        <title>Rio Bagas Pamungkas</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Custom styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+        <link href="fonts/antonio-exotic/stylesheet.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/lightbox.min.css">
+        <link href="css/responsive.css" rel="stylesheet">
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/lightbox-plus-jquery.min.js" type="text/javascript"></script>
+        <script src="js/instafeed.min.js" type="text/javascript"></script>
+        <script src="js/custom.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div id="page">
+            <!--header--->
+            <header class="header-container">
+                <div class="container">
+                    <div class="top-row">
+                        <div class="row">
+                            <div class="col-md-2 col-sm-6 col-xs-6">
+                                <div id="logo">
+                                    <!--<a href="home.html"><img src="images/logo.png" alt="logo"></a>-->
+                                    <img src="images/logo.png" background-position="center" background-size="38px" display="inline-block" width="180px" margin-top="8px" float="left" margin-right="10px" >
+                                </div>                       
+                            </div>
+                            <div class="col-md-8 col-sm-12 col-xs-12 remove-padd">
+                                <nav class="navbar navbar-default">
+                                    <div class="navbar-header page-scroll">
+                                        <button data-target=".navbar-ex1-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                        </button>
+
+                                    </div>
+                                    <div class="collapse navigation navbar-collapse navbar-ex1-collapse remove-space">
+                                        <ul class="list-unstyled nav1 cl-effect-10">
+                                            <li><a data-hover="Home" href="index.php"><span>Home</span></a></li>
+                                            <li><a data-hover="About"  class="active"><span>About</span></a></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <section class="about-block">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 about-right">
+                            <td style="border:1px solid #000000;" align="center"><img src="images/foto.jpg" width="300" /><br /></td>
+                            <p><h3></h3></p>
+                            <span></span>
+                            <ul class="list-unstyled list-inline">
+                            </ul>
+                        </div>
+                        <div class="col-md-8 about-right">
+                            <p><h3>Data Diri Saya</h3></p>
+                            <p>
+                                Nama : Rio Bagas Pamungkas
+                            </p>
+                            <p>
+                                NIM : 195410232
+                            </p>
+                            <p>
+                                Tempat Lahir : Yogyakarta
+                            </p>
+                        </div>
+                    </div>
+                <div class="clearfix"> </div>
+            </div>
+        </section>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,110 @@
+<!--Template Name: vacayhome
+File Name: home.html
+Author Name: ThemeVault
+Author URI: http://www.themevault.net/
+License URI: http://www.themevault.net/license/-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="images/icons/logo.png"/>
+        <title>Rio Bagas Pamungkas</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Custom styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+        <link href="fonts/antonio-exotic/stylesheet.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/lightbox.min.css">
+        <link href="css/responsive.css" rel="stylesheet">
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/lightbox-plus-jquery.min.js" type="text/javascript"></script>
+        <script src="js/instafeed.min.js" type="text/javascript"></script>
+        <script src="js/custom.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div id="page">
+            <!--header--->
+            <header class="header-container">
+                <div class="container">
+                    <div class="top-row">
+                        <div class="row">
+                            <div class="col-md-2 col-sm-6 col-xs-6">
+                                <div id="logo">
+                                    <!--<a href="home.html"><img src="images/logo.png" alt="logo"></a>-->
+                                    <img src="images/logo.png" background-position="center" background-size="38px" display="inline-block" width="180px" margin-top="8px" float="left" margin-right="10px" >
+                                </div>                       
+                            </div>
+                            <div class="col-md-8 col-sm-12 col-xs-12 remove-padd">
+                                <nav class="navbar navbar-default">
+                                    <div class="navbar-header page-scroll">
+                                        <button data-target=".navbar-ex1-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                        </button>
+
+                                    </div>
+                                    <div class="collapse navigation navbar-collapse navbar-ex1-collapse remove-space">
+                                        <ul class="list-unstyled nav1 cl-effect-10">
+                                            <li><a  data-hover="Home" class="active"><span>Home</span></a></li>
+                                            <li><a data-hover="ABOUT"  href="about.php"><span>ABOUT</span></a></li>
+                                        </ul>
+
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+
+            <!--end-->
+            <div id="myCarousel1" class="carousel slide" data-ride="carousel"> 
+                <!-- Indicators -->
+
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel1" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel1" data-slide-to="1"></li>
+                    <li data-target="#myCarousel1" data-slide-to="2"></li>
+                    <li data-target="#myCarousel1" data-slide-to="3"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active"> <img src="images/banner.jpg" style="width:100%;" alt="First slide">
+                        <
+                        <div class="carousel-caption">
+                            <h1><br> </h1>
+                        </div>
+                    </div>
+                    <div class="item"> <img src="images/banner2.jpg" style="width:100%;" alt="Second slide">
+                        <div class="carousel-caption">
+                            <h1><br> </h1>
+                        </div>
+                    </div>
+                    <div class="item"> <img src="images/banner3.jpg" style="width:100%;" alt="Third slide">
+                        <div class="carousel-caption">
+                            <h1><br> </h1>
+                        </div>
+                    </div>
+                    <div class="item"> <img src="images/banner4.jpg" style="width:100%;" alt="Third slide">
+                        <div class="carousel-caption">
+                            <h1><br> </h1>
+                        </div>
+                    </div>
+
+                </div>
+                <a class="left carousel-control" href="#myCarousel1" data-slide="prev"> <img src="images/icons/left-arrow.png" onmouseover="this.src = 'images/icons/left-arrow-hover.png'" onmouseout="this.src = 'images/icons/left-arrow.png'" alt="left"></a>
+                <a class="right carousel-control" href="#myCarousel1" data-slide="next"><img src="images/icons/right-arrow.png" onmouseover="this.src = 'images/icons/right-arrow-hover.png'" onmouseout="this.src = 'images/icons/right-arrow.png'" alt="left"></a>
+            </div>
+    </body>
+</html>
+
+
+            
